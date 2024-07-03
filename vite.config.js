992001f4abc914e3498594b73ade4058aa70bc9f
thejakumari <theja.xworkz@gmail.com>
2024-07-03 22:25:15 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  async configureServer(server) {
+    const { data } = await axios.get('https://backend-task-5c19vhidx-theja12345678s-projects.vercel.app');
+    const dataPath = path.resolve(__dirname, 'public', 'data.json');
+    fs.writeFileSync(dataPath, JSON.stringify(data));
+  }
 })
